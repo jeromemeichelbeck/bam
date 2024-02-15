ac9eacd64bdbb51c69b696598c1c0e9f56ff9e7f
Jérôme Meichelbeck <jerome.meichelbeck@bionext.lu>
2024-02-15 23:19:37 +0100

    Transfert form back button on first step

--- src/components/transfert/TransfertStepper.tsx
+++ src/components/transfert/TransfertStepper.tsx
@@ -1,3 +1,5 @@
+import { useAccountId } from "@/hooks/useAccountId";
+import { useOwnerId } from "@/hooks/useOwnerId";
 import { TransfertFormDTO } from "@/schemas/transfert";
 import {
   Box,
@@ -7,6 +9,7 @@ import {
   Stepper,
   Typography,
 } from "@mui/material";
+import { useRouter } from "next/router";
 import { FC, useState } from "react";
 import { Control } from "react-hook-form";
 import AmountStep from "./AmountStep";
@@ -39,14 +42,28 @@ const TransfertStepper: FC<TransfertStepperProps> = ({
   resetToAccountId,
   control,
 }) => {
+  const router = useRouter();
   const [activeStep, setActiveStep] = useState(0);
+
+  const { ownerId } = useOwnerId();
+  const { accountId } = useAccountId();
 
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
   const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    if (activeStep > 0) {
+      return setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    }
+
+    if (accountId) {
+      return router.push(`/account/${accountId}`);
+    }
+
+    if (ownerId) {
+      return router.push(`/owner/${ownerId}`);
+    }
   };
 
   const handleReset = () => {
@@ -85,11 +102,11 @@ const TransfertStepper: FC<TransfertStepperProps> = ({
           <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
             <Button
               color="inherit"
-              disabled={activeStep === 0}
+              disabled={activeStep === 0 && !accountId && !ownerId}
               onClick={handleBack}
               sx={{ mr: 1 }}
             >
-              Back
+              {activeStep > 0 ? "Back" : "Cancel"}
             </Button>
             <Box sx={{ flex: "1 1 auto" }} />
             <Button onClick={handleNext}>
